3395f5d73b5cc137b1a12f74e9a82c76347e1173
Dark_Devil <162443495+darkdevil120@users.noreply.github.com>
2024-10-13 12:26:40 +0545

    Delete index.js
    Signed-off-by: Dark_Devil <162443495+darkdevil120@users.noreply.github.com>

--- index.js
+++
@@ -1,8 +0,0 @@
-// Platform detection logic
-    if (window.Telegram && window.Telegram.WebApp) {
-      // Telegram environment detected
-      loadTelegramGame();
-    } else {
-      // Default to website environment
-      loadWebsiteGame();
-    }
